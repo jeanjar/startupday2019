23459847e0e3a40ee681ff25a23075fd222eb35a
Jean Jar Pereira de Araújo <jeanjpa@gmail.com>
2019-05-17 19:48:35 -0300

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel + Deployer
+                    Laravel + Deployer + !
                 </div>
 
                 <div class="links">
