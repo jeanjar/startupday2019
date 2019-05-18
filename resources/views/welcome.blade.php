e169d6a50bc4eba21b65ad7e6f2cfcb7bd506390
Jean Jar Pereira de Araújo <jeanjpa@gmail.com>
2019-05-18 16:00:42 -0300

    brasil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel + PHPRN
+                    Laravel + PHPRN + PHP BRASIL
                 </div>
 
                 <div class="links">
